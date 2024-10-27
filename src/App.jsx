0686d8f5e87b62b9ca9ece9947a5b3a457f12da1
Aden Gonzales <adenjgz@gmail.com>
2024-10-26 21:17:13 -0500

    update 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,24 @@ import musicNote from '../public/music_15509.png'
 import './App.css'
 
 function App() {
-//    const [input1, setInput1] = useState('')
-//    const [input2, setInput2] = useState('')
+   const [input1, setInput1] = useState('')
+   const [input2, setInput2] = useState('')
 
-//    const handleChange = (event) => {
-//    const { name , value} = event.target;
-   
-//    if (name === 'input1') {
-//     setInput1(value);
-//   } else if (name === 'input2') {
-//     setInput2(value);
-//   }
-// };
+   const handleChange = (event) => {
+    const { name , value} = event.target;
+
+    
+    if (name === 'input1') {
+      setInput1(value);
+    } else if (name === 'input2') {
+      setInput2(value);
+    }
+  };
 
   return (
     <>
       <div>   
-        <a href="https://react.dev" target="_blank">
+        <a href="" target="_blank">
           <img src={musicNote} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -29,17 +30,25 @@ function App() {
       Input song name here
       <div className="card">
 
-       {/* <input type="text" value={input1} onChange={handleChange} />
-      <p>You entered: {input1}</p>  */}
+       <input type="text" value={input1} onChange={(e) => setInput1(e.target.value)} name='input1' />
+      <p>You entered: {input1}</p>
         <p>
           Input Artist name here
         </p>
-        {/* <input type="text" value={input2} onChange={handleChange} />
-      <p>You entered: {input2}</p>  */}
+      {/* <input type="text" value={input2} onChange={handleChange} name='input2' />
+      <p>You entered: {input2}</p> */}
       </div>
       <p className="read-the-docs">
         Stample will find the time stamps of samples in music
       </p>
+
+      <form action="/action_page.php">
+        <label for="fname">Song name:</label><br/>
+        <input type="text" id="fname" name="fname" value=""/><br/>
+        <label for="lname">Artist name:</label><br/>
+        <input type="text" id="lname" name="lname" value=""/><br/><br/>
+        <input type="submit" value="Submit"/>
+      </form>
     </>
   )
 }
