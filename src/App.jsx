ea99ac512e6ba26db9c8887d03be6a41c6e58e50
Aden Gonzales <adenjgz@gmail.com>
2024-10-26 20:03:45 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,18 @@ import musicNote from '../public/music_15509.png'
 import './App.css'
 
 function App() {
-   const [inputValue, setInputValue] = useState('')
+//    const [input1, setInput1] = useState('')
+//    const [input2, setInput2] = useState('')
 
-   const handleChange = (event) => {
-   setInputValue(event.target.value);
-   }
+//    const handleChange = (event) => {
+//    const { name , value} = event.target;
+   
+//    if (name === 'input1') {
+//     setInput1(value);
+//   } else if (name === 'input2') {
+//     setInput2(value);
+//   }
+// };
 
   return (
     <>
@@ -19,13 +26,16 @@ function App() {
         </a>
       </div>
       <h1>STAMPLE</h1>
-      hi
+      Input song name here
       <div className="card">
-      <input type="text" value={inputValue} onChange={handleChange} />
-      <p>You entered: {inputValue}</p> 
+
+       {/* <input type="text" value={input1} onChange={handleChange} />
+      <p>You entered: {input1}</p>  */}
         <p>
-          Input song name here
+          Input Artist name here
         </p>
+        {/* <input type="text" value={input2} onChange={handleChange} />
+      <p>You entered: {input2}</p>  */}
       </div>
       <p className="read-the-docs">
         Stample will find the time stamps of samples in music
