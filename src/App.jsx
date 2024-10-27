13068322786955cd43d51ecb5c1237b751fb178b
Aden Gonzales <adenjgz@gmail.com>
2024-10-26 19:15:22 -0500

    Website 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import musicNote from '../public/music_15509.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [inputValue, setInputValue] = useState('')
+
+   const handleChange = (event) => {
+   setInputValue(event.target.value);
+   }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+      <div>   
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={musicNote} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>STAMPLE</h1>
+      hi
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          the count is {count}
-        </button>
+      <input type="text" value={inputValue} onChange={handleChange} />
+      <p>You entered: {inputValue}</p> 
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Input song name here
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Stample will find the time stamps of samples in music
       </p>
     </>
   )
